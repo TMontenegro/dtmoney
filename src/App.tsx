b083ff7bfef97b883a161a7e12323fc2d7c1a07a
Tales Montenegro <montenegro.tales@gmail.com>
2021-09-23 23:34:09 -0300

    style(global): setup and implement global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
       <h1>App works!</h1>
+      <GlobalStyle />
     </div>
   );
 }
